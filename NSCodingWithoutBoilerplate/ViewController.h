53170c0ff3b9a68ec772d1a85867cfe358ebfe4a
pencilCool <yhtangcoder@gmail.com>
2017-06-14 07:55:07 +0800

    Initial Commit

---
+++ NSCodingWithoutBoilerplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSCodingWithoutBoilerplate
+//
+//  Created by pencilCool on 2017/6/14.
+//  Copyright © 2017年 pencilCool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
